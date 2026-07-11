97c2fdcd2526d1bbf399a060fea92592e38caa0e
Jaeyeon Jo <jojaeyeon.kr@gmail.com>
2026-07-11 13:08:33 +0900

    When connnection is closed, try to reconnect.

--- monitoring_agent/tcp_client.go
+++ monitoring_agent/tcp_client.go
@@ -11,21 +11,49 @@ import (
 
 const connectAddr = "127.0.0.1:4567"
 
+func tryConnect(conn *net.Conn, sigChan chan os.Signal) error {
+	var err error
+
+	for {
+		connected := false
+		select {
+		case sig := <-sigChan:
+			fmt.Printf("Sig interrupted while dialing: %v\n", sig)
+			err = fmt.Errorf("Sig interrupted while dialing: %v\n", err)
+			return err
+		default:
+			*conn, err = net.Dial("tcp", connectAddr)
+			if err != nil {
+				fmt.Printf("dial error: %v, try to reconnect after 5 sec.\n", err)
+				time.Sleep(5 * time.Second)
+				continue
+			} else {
+				connected = true
+
+			}
+		}
+		if connected {
+			break
+		}
+	}
+	return err
+}
+
 func RunClient() {
+	var conn net.Conn
+	var err error
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
-	conn, err := net.Dial("tcp", connectAddr)
-
-	if err != nil {
-		fmt.Printf("error %s\n", err)
-		return
-	}
-
-	defer conn.Close()
-
 	ticker := time.NewTicker(3 * time.Second)
 	defer ticker.Stop()
+
+	err = tryConnect(&conn, sigChan)
+	if err != nil {
+		fmt.Printf("Connection error %s\n", err)
+		return
+	}
+	defer conn.Close()
 
 	cpuInfo, err := GetCpuInfo()
 	if err != nil {
@@ -35,10 +63,6 @@ func RunClient() {
 	cpuInfoMsg := CpuInfoToStr(cpuInfo)
 	n, err := conn.Write([]byte(cpuInfoMsg))
 
-	if err != nil {
-		fmt.Printf("Cpu Info Sending error %s\n", err)
-		return
-	}
 	fmt.Printf("Size of sended packet : %d.\n", n)
 
 	for {
@@ -53,7 +77,14 @@ func RunClient() {
 			n, err := conn.Write([]byte(resourceStatus))
 			if err != nil {
 				fmt.Printf("Write resource status error %s\n", err)
-				break
+				conn.Close()
+				err = tryConnect(&conn, sigChan)
+				if err != nil {
+					fmt.Printf("Signal Interruped %s\n", err)
+					return
+				} else {
+					defer conn.Close()
+				}
 			}
 			fmt.Printf("sended packet len : %d\n", n)
 		case sig := <-sigChan:
